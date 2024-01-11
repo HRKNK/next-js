4aede8e6823638fb888013fb8dbb870e6619dabc
HRKNK <nikita.zyamzin@yandex.ru>
2024-01-11 17:09:19 +0300

    update

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,3 +1,6 @@
+'use client';
+// определение клиентского компонента (будет отправлен клиенту)
+
 import classNames from 'classnames';
 import styles from './Button.module.css';
 import { IButton } from './Button.types';
@@ -5,7 +8,7 @@ import { IButton } from './Button.types';
 const Button = ({children, appearance = 'primary', className, ...props}: IButton): JSX.Element => {
     return (
         // classNames принимает дэфолт стили + условный объект
-        <button type='button' className={classNames(styles.button, className, {
+        <button onClick={()=> console.log('SHIT')} type='button' className={classNames(styles.button, className, {
             [styles.primary]: appearance == 'primary',
             [styles.ghost]: appearance == 'ghost',
         })} 
