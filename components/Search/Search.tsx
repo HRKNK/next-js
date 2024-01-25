4288b0750fbaaa7e6b82287bf31352183964d729
HRKNK <nikita.zyamzin@yandex.ru>
2024-01-25 15:33:45 +0300

    update

---
+++ components/Search/Search.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { ISearch } from './Search.types';
+import styles from './Search.module.css';
+import Lens from '@/public/lens.svg';
+
+const Search = ({...props}: ISearch) => {
+    const [value, setValue] = useState<string>('');
+
+    return (
+        <div className={styles.search__wrapper}>
+            <input 
+                className={styles.input__search} 
+                placeholder={'Поиск...'} 
+                type='text'
+                value={value} 
+                onChange={e => setValue(e.target.value)}
+                {...props}
+            />
+            <button className={styles.button__search} type='button'><Lens fill="white"/></button>
+        </div>
+    );
+};
+
+export default Search;
---
+++ components/Search/Search.types.ts
@@ -0,0 +1,5 @@
+import { DetailedHTMLProps, HTMLAttributes, InputHTMLAttributes } from "react";
+
+export interface ISearch extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
+
+}
