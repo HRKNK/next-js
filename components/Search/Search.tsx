df191851df22431bc8738ecf99838a7c4c9c1995
HRKNK <nikita.zyamzin@yandex.ru>
2024-01-31 17:01:44 +0300

    update

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { type ChangeEvent, useEffect, useState } from 'react';
 import { ISearch } from './Search.types';
 import styles from './Search.module.css';
 import Lens from '@/public/lens.svg';
@@ -9,6 +9,13 @@ import classNames from 'classnames';
 const Search = ({button, target, className, ...props}: ISearch): JSX.Element => {
     const [value, setValue] = useState<string>('');
     const [focus, setFocus] = useState<boolean>(false);
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+        // if (e.target.value.length >= 1) {
+            // TODO send response
+        // }
+    }
 
     return (
         <div className={classNames(styles.search__wrapper, className, {
@@ -22,10 +29,10 @@ const Search = ({button, target, className, ...props}: ISearch): JSX.Element => 
                 placeholder={target} 
                 type='text'
                 value={value} 
-                onChange={e => setValue(e.target.value)}
+                onChange={onChange}
                 {...props}
             />
-            {button && <button className={styles.button__search} type='button'><Lens fill="white"/></button>}
+            {button && <button onClick={()=> console.log('Click!')} className={styles.button__search} type='button'><Lens fill="white"/></button>}
         </div>
     );
 };
