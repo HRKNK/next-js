44ae3542a07ed5e2884edb4288cc14966ccf2268
HRKNK <nikita.zyamzin@yandex.ru>
2024-03-18 15:58:25 +0300

    upd

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,33 +1,36 @@
 // Корневой маршрут :host/
 // page.tsx - зарезервированное имя маршрутизации
 
-import styles from './page.module.css'
-import { Metadata } from 'next/types';
-import Header from '@/layouts/header/Header';
-import Sidebar from '@/layouts/sidebar/Sidebar';
-import Footer from '@/layouts/footer/Footer';
-import { getMenu } from '@/api/page';
-import Menu from '@/widgets/menu/Menu';
-import { Htag } from '@/components/public.api';
-import Tag from '@/components/Tag/Tag';
-import Sort from '@/components/Sort/Sort';
+import styles from "./page.module.css";
+import { Metadata } from "next/types";
+import Header from "@/layouts/header/Header";
+import Sidebar from "@/layouts/sidebar/Sidebar";
+import Footer from "@/layouts/footer/Footer";
+import { getMenu } from "@/api/page";
+import Menu from "@/widgets/menu/Menu";
+import { Htag } from "@/components/public.api";
+import Tag from "@/components/Tag/Tag";
+import Sort from "@/components/Sort/Sort";
 
+// Вычисляемые метаданные (или мета-данные подгруженные по запросу)
 // generateMetadata зарезервированное имя
-// вычисляемые метаданные
-export async function generateMetadata (): Promise<Metadata> {
-  // fetch запрос (аки асинхрон)
+export async function generateMetadata(): Promise<Metadata> {
+  // Делаем fetch-запрос (аки асинхрон):
+  // const fetch = await fetch('host', GET)
   return {
-	title: 'My application',
-	icons: './url.ico', // имеет более низкий приоритет перед файловой структурой favicon.ico
-  }
+    title: "My application",
+    icons: "./url.ico", // имеет более низкий приоритет перед файловой структурой favicon.ico
+    openGraph: {
+      description: "OG",
+    },
+    // вставляем результат запроса ...fetch
+  };
 }
 
 export default async function Home() {
-	return (
-		// <AppContextProvider menu={menu} firstCategory={TopLevelCategory.Courses}>
-		<div>
-			{"Это главная страница"}
-		</div>
-		// </AppContextProvider>
-	)
+  return (
+    // <AppContextProvider menu={menu} firstCategory={TopLevelCategory.Courses}>
+    <div>{"Это главная страница"}</div>
+    // </AppContextProvider>
+  );
 }
